c89deab1476c357130f16aefef21f97572d6663a
David Roberts <david.roberts@puppetlabs.com>
2015-04-29 18:25:55 -0500

    removed dummy groups

--- manifests/groups.pp
+++ manifests/groups.pp
@@ -1,6 +1,3 @@
 class tse_admins::groups {
   @group { 'tseadmin': ensure => present, gid => 600 }
-  @group { 'foo':      ensure => present, gid => 601 }
-  @group { 'bar':      ensure => present, gid => 602 }
-  @group { 'foobar':   ensure => present, gid => 603 }
 }
